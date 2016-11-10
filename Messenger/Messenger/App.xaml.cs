0820691bf3acc0f446ada7baf48d97b131b087ec
ErmilovaMarie <ermilova.m98@mail.ru>
2016-11-10 22:16:47 +0300

    Third commit

--- Messenger/Messenger/App.xaml.cs
+++ Messenger/Messenger/App.xaml.cs
@@ -14,7 +14,7 @@ namespace Messenger
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage = new NavigationPage (new LoginPage());
         }
 
         protected override void OnStart()
