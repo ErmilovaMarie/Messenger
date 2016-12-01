7c2993f7eec4c75719418a3a001e71ebefe7a12c
ErmilovaMarie <ermilova.m98@mail.ru>
2016-12-01 19:41:22 +0300

    laba4_1 commit

---
+++ Messenger/Messenger/Services/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Services
+{
+    class DataService
+    {
+        private static readonly DataService _dataservice = new DataService();
+
+        protected DataService()
+        {
+
+        }
+
+        public static DataService GetInstance ()
+        {
+            return _dataservice;
+        }
+    }
+}
