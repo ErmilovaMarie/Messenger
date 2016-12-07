ba556725e7ed37d4a2bb111ff23f21c9390becb7
ErmilovaMarie <ermilova.m98@mail.ru>
2016-12-07 23:48:16 +0300

    DataServiceChanged

--- Messenger/Messenger/Services/DataService.cs
+++ Messenger/Messenger/Services/DataService.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Messenger.Services
 {
     class DataService
     {
-        private static readonly DataService _dataservice = new DataService();
+        private static readonly DataService _dataService = new DataService();
+
+        private readonly HttpClient _httpClient = new HttpClient
+        {
+            DefaultRequestHeaders = { IfModifiedSince = DateTimeOffset.Now }
+        };
+
+        private readonly string _baseUrl = "http://127.0.0.1:9000";
 
         protected DataService()
         {
@@ -17,7 +27,35 @@ namespace Messenger.Services
 
         public static DataService GetInstance ()
         {
-            return _dataservice;
+            return _dataService;
+        }
+
+        public async Task<HttpStatusCode> LoginAsync(string userName, string password)
+        {
+            try
+            {
+                // Объявляем пространства имён
+                XNamespace defaultNamespace = "http://schemas.servicestack.net/types";
+                XNamespace instanceNamespace = "http://www.w3.org/2001/XMLSchema-instance";
+
+                // Создаём корневой элемент с атрибутами и добавляем в документ
+                var xmlDocument = new XDocument();
+                var auth = new XElement(defaultNamespace + "Auth");
+                auth.Add(new XAttribute(XNamespace.Xmlns + "i", instanceNamespace));
+                auth.Add(new XElement(defaultNamespace + "UserName", userName));
+                auth.Add(new XElement(defaultNamespace + "Password", password));
+                xmlDocument.Add(auth);
+
+                var xml = xmlDocument.ToString();
+
+                return (await _httpClient.PostAsync(new Uri($"{_baseUrl}/auth/credentials"),
+                    new StringContent(xml, Encoding.UTF8, "application/xml"))).StatusCode;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return HttpStatusCode.SeeOther;
+            }
         }
     }
 }
